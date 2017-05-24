1546639a921960c50405706f850357d0f5eafee6
Kumar <Vijay.Kumar4@emc.com>
2017-05-23 19:10:20 -0700

    testing

--- collector.rb
+++ collector.rb
@@ -221,9 +221,9 @@ config['unisphere'].each do |unisphere|
               metrics_param = get_metrics(monitor['children'][0]['scope'],myparams)
               metric_payload = build_metric_payload(unisphere,monitor,symmetrix,metrics_param,key,parent_ids,child_key,child_ids)
               metrics = get_perf_metrics(unisphere,metric_payload,monitor['children'][0],auth)
+              puts "this is metrics" + metrics
               metrics_param.each do |metric|
-                influx_output_payload << {series: metric, tags: { array_type: 'symmetrix', sn: symmetrix['sid'], component: monitor['scope'], component_name: key[parent_ids[0]], child_component_name: child_key[child_ids[0]]}, values: { value: metrics[metric] } } 
-                #influx_output_payload.push({series: metric, tags: { array_type: 'symmetrix', sn: symmetrix['sid'], component: monitor['scope'], component_name: key[parent_ids[0]], child_component_name: child_key[child_ids[0]]}, values: { value: metrics[metric] } } )
+                influx_output_payload.push({series: metric, tags: { array_type: 'symmetrix', sn: symmetrix['sid'], component: monitor['scope'], component_name: key[parent_ids[0]], child_component_name: child_key[child_ids[0]]}, values: { value: metrics[metric] } } )
                 graphite_output_payload[(config['graphite']['prefix'] ? "#{config['graphite']['prefix']}." : "") + "symmetrix.#{symmetrix['sid']}.#{monitor['scope']}.#{key[parent_ids[0]]}.#{child_key[child_ids[0]]}.#{metric}"] = metrics[metric]
               end
             end
@@ -234,11 +234,9 @@ config['unisphere'].each do |unisphere|
           metric_payload = build_metric_payload(unisphere,monitor,symmetrix,metrics_param,key,parent_ids)
           metrics = get_perf_metrics(unisphere,metric_payload,monitor,auth)
           metrics_param.each do |metric|
-            influx_output_payload.push << {series: metric, tags: { array_type: 'symmetrix', sn: symmetrix['sid'], component: monitor['scope'], component_name: key[parent_ids[0]]}, values: { value: metrics[metric] } }  unless monitor['scope'] == "Array"
-            puts metric
-            influx_output_payload << {series: metric, tags: { array_type: 'symmetrix', sn: symmetrix['sid'], component: monitor['scope']}, values: { value: metrics[metric] } }  if monitor['scope'] == "Array"
-            #influx_output_payload.push({series: metric, tags: { array_type: 'symmetrix', sn: symmetrix['sid'], component: monitor['scope'], component_name: key[parent_ids[0]]}, values: { value: metrics[metric] } } ) unless monitor['scope'] == "Array"
-            #influx_output_payload.push({series: metric, tags: { array_type: 'symmetrix', sn: symmetrix['sid'], component: monitor['scope']}, values: { value: metrics[metric] } } ) if monitor['scope'] == "Array"
+            puts "this is metrics" + metric
+            influx_output_payload.push({series: metric, tags: { array_type: 'symmetrix', sn: symmetrix['sid'], component: monitor['scope'], component_name: key[parent_ids[0]]}, values: { value: metrics[metric] } } ) unless monitor['scope'] == "Array"
+            influx_output_payload.push({series: metric, tags: { array_type: 'symmetrix', sn: symmetrix['sid'], component: monitor['scope']}, values: { value: metrics[metric] } } ) if monitor['scope'] == "Array"
             #graphite_output_payload[(config['graphite']['prefix'] ? "#{config['graphite']['prefix']}." : "") + "symmetrix.#{symmetrix['sid']}.#{monitor['scope']}.#{metric}"] = metrics[metric] if monitor['scope'] == "Array"
             #graphite_output_payload[(config['graphite']['prefix'] ? "#{config['graphite']['prefix']}." : "") + "symmetrix.#{symmetrix['sid']}.#{monitor['scope']}.#{key[parent_ids[0]]}.#{metric}"] = metrics[metric] unless monitor['scope'] == "Array"
           end
